d4a2c8d49dd9ee8d3fd734289ddbfd700f82515c
alifarhan786 <alifarhan358@gmail.com>
2024-10-02 18:58:41 +0530

    Chunk size increase

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     // Adjust the warning limit (in kilobytes)
-    chunkSizeWarningLimit: 1000, // Change this value as needed
+    chunkSizeWarningLimit: 2000, // Adjust this value as needed
   },
-})
+});
